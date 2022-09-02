1fc26ac1cc054319122c22579a8aa8634d263a56
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-09-02 02:06:58 -0300

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -62,12 +62,12 @@ const Home: React.FC = () => {
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           <span style={{ fontFamily: 'Ostrich Sans', textTransform: 'uppercase', fontSize: '40pt' }}>
-            {TranslateString(576, 'In-Dex Sale')}
+            {TranslateString(576, '')}
           </span>
         </Heading>
         <Text>
           <span style={{ color: '#d94590' }}>
-            {TranslateString(578, 'Most good exchange in all of Binance Smart Chain!')}
+            {TranslateString(578, '')}
           </span>
         </Text>
       </Hero>
