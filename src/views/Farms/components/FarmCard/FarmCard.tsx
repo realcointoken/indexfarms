14a0767eb4e0a897b0bc9e655c6d744f8d0b8965
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-01-15 02:53:09 -0300

    Update FarmCard.tsx

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -121,7 +121,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
 
   const lpLabel = farm.lpSymbol
   const earnLabel = 'BullChain'
-  const farmAPY = 150
+  const farmAPY = '150'
     farm.apy &&
     farm.apy.times(new BigNumber(100)).toNumber().toLocaleString(undefined, {
       minimumFractionDigits: 2,
