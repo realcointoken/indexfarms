7ab5c820016a3935de1835c78ee9e57425854a7b
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-03-07 17:38:12 -0300

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'BullChainGlobal',
+            screenName: 'bullchainai',
           }}
           options={{
             height: '300',
