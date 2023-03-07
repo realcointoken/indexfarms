97786cf1831dd1bfc455a8910afb5e6f45f1f7df
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-03-07 03:08:57 -0300

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -1,26 +1,10 @@
 import contracts from './contracts'
 import { FarmConfig, QuoteToken } from './types'
 
-const TACO_BUSD_LP_MAINNET = `0x3BB3B98c0FABE30166E6B7D62aEef393249871E4`
 const TACO_BNB_LP_MAINNET = `0x5590d45592C846DD820D360dd5Ba6b2610cDeeFB`
 const TACO_MAINNET = `0x841E83120462945F32a6687AD28a5E7Bee023169`
 
 const farms: FarmConfig[] = [
-  {
-    risk: 5,
-    lpSymbol: 'BullChain-BUSD LP',
-    lpAddresses: {
-      97: '',
-      56: '0x841E83120462945F32a6687AD28a5E7Bee023169',
-    },
-    tokenSymbol: '$TACO',
-    tokenAddresses: {
-      97: '',
-      56: '0x841E83120462945F32a6687AD28a5E7Bee023169',
-    },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
-  },
   {
     risk: 5,
     lpSymbol: 'BullChain-BNB LP',
@@ -39,7 +23,7 @@ const farms: FarmConfig[] = [
   {
     risk: 5,
     isTokenOnly: true,
-    lpSymbol: '$CFT',
+    lpSymbol: 'BullChain',
     lpAddresses: {
       97: '',
       56: TACO_BUSD_LP_MAINNET, // BullChain-BUSD LP
