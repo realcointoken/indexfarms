c4267e5706308ee177d0b1765a9421c2de39ed58
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-03-07 17:35:58 -0300

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -3,7 +3,7 @@ import { FarmConfig, QuoteToken } from './types'
 
 const TACO_BUSD_LP_MAINNET = `0x3BB3B98c0FABE30166E6B7D62aEef393249871E4`
 const TACO_BNB_LP_MAINNET = `0x5590d45592C846DD820D360dd5Ba6b2610cDeeFB`
-const TACO_MAINNET = `0x841E83120462945F32a6687AD28a5E7Bee023169`
+const TACO_MAINNET = `0x62B427DaA6DCe1bea02F74AE09f6b7e82Bd860e5`
 
 const farms: FarmConfig[] = [
   {
@@ -11,12 +11,12 @@ const farms: FarmConfig[] = [
     lpSymbol: 'BullChain-BUSD LP',
     lpAddresses: {
       97: '',
-      56: '0x841E83120462945F32a6687AD28a5E7Bee023169',
+      56: '0x62B427DaA6DCe1bea02F74AE09f6b7e82Bd860e5',
     },
     tokenSymbol: '$TACO',
     tokenAddresses: {
       97: '',
-      56: '0x841E83120462945F32a6687AD28a5E7Bee023169',
+      56: '0x62B427DaA6DCe1bea02F74AE09f6b7e82Bd860e5',
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
@@ -26,7 +26,7 @@ const farms: FarmConfig[] = [
     lpSymbol: 'BullChain-BNB LP',
     lpAddresses: {
       97: '',
-      56: '0x841E83120462945F32a6687AD28a5E7Bee023169',
+      56: '0x62B427DaA6DCe1bea02F74AE09f6b7e82Bd860e5',
     },
     tokenSymbol: '$TACO',
     tokenAddresses: {
